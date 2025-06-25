76746e5e8cb8d977d99226b7bab679692e008cf6
CharlieHelps <charlie@charlielabs.ai>
2025-06-25 18:08:46 +0000

    chore: remove temporary home/ and tmp/ directories

--- tmp/mapping2.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, Of } from '../src/schema';
-
-class U extends Schema.from({
-  active: Of<boolean>({ default: true }),
-  id: Of<string>({}),
-}) {}
-
-type C = ConstructorParameters<typeof U>[0];
--- tmp/mapping3.ts
+++
@@ -1,5 +0,0 @@
-import { Schema, Of } from '../src/schema';
-
-class Model extends Schema.from({ flag: Of<boolean>({ default: true }) }) {}
-
-const m = new Model({});
