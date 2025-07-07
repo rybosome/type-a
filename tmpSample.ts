6d9e7be93bec48ef637c37ba5663ff74604ad42a
Ryan Eiger <eiger.ryan@gmail.com>
2025-07-07 14:57:09 -0700

    Delete tmpSample.ts

--- tmpSample.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, Of } from "@rybosome/type-a";
-
-class LoginAttempt extends Schema.from({
-  success: Of<boolean>(),
-}) {}
-
-class User extends Schema.from({
-  loginAttempts: Of<LoginAttempt[]>(),
-}) {}
